e382d444195b083dc6134ce1b377e98f12d94ebc
SHUBH PORWAL <114678897+SHUBH-PORWAL@users.noreply.github.com>
2025-08-14 18:58:21 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://shubhporwal:07HLTAGoS32d1iFo@cluster0.vmkrjop.mongodb.net/test");
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('Database connection error:', error);
