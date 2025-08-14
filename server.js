202fa8958b566d3f245a6a464570935475c1787a
SHUBH PORWAL <114678897+SHUBH-PORWAL@users.noreply.github.com>
2025-08-14 18:23:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', require('./routes/auth'));
@@ -45,6 +45,7 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 
 });
+
 
 
 
